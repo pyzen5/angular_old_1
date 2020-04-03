576188eb5d179d420844ec40cb219f71cbfad1b9
pyzen5 <pyzen55@gmail.com>
2020-04-03 16:10:44 +0530

    Domain Feature Module

---
+++ src/app/customer/customer-dashboard/customer-dashboard.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CutomerDashboardComponent } from './cutomer-dashboard.component';
+
+
+
+@NgModule({
+  declarations: [CutomerDashboardComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CustomerDashboardModule { }
---
+++ src/app/customer/customer-dashboard/cutomer-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cutomer-dashboard',
+  templateUrl: './cutomer-dashboard.component.html',
+  styleUrls: ['./cutomer-dashboard.component.css']
+})
+export class CutomerDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
